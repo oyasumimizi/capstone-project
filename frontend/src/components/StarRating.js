e89dbc78308b55bc3ce133871c30604f490cfcda
brooke <daisyrakionvideos@gmail.com>
2021-02-04 21:39:08 -0600

    login component for the front end is functional. just need to find the synax error.

--- frontend/src/components/StarRating.js
+++ frontend/src/components/StarRating.js
@@ -1,35 +1,35 @@
-import React, { useState } from "react";
-import { FaStar } from "react-icons/fa";
+// import React, { useState } from "react";
+// import { FaStar } from "react-icons/fa";
 
-const StarRating = () => {
-    const [rating, setRating] = useState(null);
-    const [hover, setHover] = useState(null);
+// const StarRating = () => {
+//     const [rating, setRating] = useState(null);
+//     const [hover, setHover] = useState(null);
 
-  return (
-    <div>
-      {[ ...Array(5)].map((star, i) => {
-          const ratingValue = i + 1;
+//   return (
+//     <div>
+//       {[ ...Array(5)].map((star, i) => {
+//           const ratingValue = i + 1;
 
-        return (
-        <label>
-            <input
-             type="radio" 
-             name="rating" 
-             value={ratingValue} 
-             onClick={() => setRating(ratingValue)}
-             />
-            <FaStar className="star" 
-            color={ratingValue < (hover || rating) ? "#F3DE8F" : "#B2B2B2"}
-             size={20}
-             onMouseEnter={() => setHover(ratingValue)}
-             OnMouseLeave={() => setHover(null)}
-            /> 
-            </label>
-         );
-      })}
-      <p>The rating is {rating}</p>
-    </div>
-  );
-};
+//         return (
+//         <label>
+//             <input
+//              type="radio" 
+//              name="rating" 
+//              value={ratingValue} 
+//              onClick={() => setRating(ratingValue)}
+//              />
+//             <FaStar className="star" 
+//             color={ratingValue < (hover || rating) ? "#F3DE8F" : "#B2B2B2"}
+//              size={20}
+//              onMouseEnter={() => setHover(ratingValue)}
+//              OnMouseLeave={() => setHover(null)}
+//             /> 
+//             </label>
+//          );
+//       })}
+//       <p>The rating is {rating}</p>
+//     </div>
+//   );
+// };
 
-export default StarRating;
+// export default StarRating;
