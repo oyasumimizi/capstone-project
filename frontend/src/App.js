2f80411761debde38b34544d691ca436ff3420fd
brooke <daisyrakionvideos@gmail.com>
2021-02-09 21:44:02 -0600

    added cart page component. will be able to remove items from cart, possibly make a successful transaction.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
 import UploadProductPage from './views/UploadProductPage/UploadProductPage'
-import DetailProductPage from './views/DetailProductPage/DetailProductPage';
+// import DetailProductPage from './views/DetailProductPage/DetailProductPage';
 import CartPage from './views/CartPage/CartPage';
 import HistoryPage from './views/HistoryPage/HistoryPage';
 
@@ -22,7 +22,7 @@ function App() {
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} />
-          <Route exact path="/product/:productId" component={Auth(DetailProductPage, null)} />
+          {/* <Route exact path="/product/:productId" component={Auth(DetailProductPage, null)} /> */}
           <Route exact path="/user/cart" component={Auth(CartPage, true)} />
           <Route exact path="/history" component={Auth(HistoryPage, true)} />
 
---
+++ frontend/src/components/views/Footer/Footer.js
@@ -0,0 +1,1 @@
+// Worldwide shipping. Secure payments. Free returns. Local support 24 hours.
