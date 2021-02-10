86c8f1394bc2dc4e27fcfbbec6267388d86b7040
brooke <daisyrakionvideos@gmail.com>
2021-02-09 20:45:28 -0600

    added user reducer, and user actions on the front end. the components are for setting and triggering payloads for the front end. such as payment, logging in.

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,18 @@ app.use('/api/validate', validate)
 app.use("/api/products", products);
 //endpoint to login is /user/login
 
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use('/uploads', express.static('uploads'));
+
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
+
 var port = process.env.PORT || '5000'
 app.listen(port, err => {
     if (err)
